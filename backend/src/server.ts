a98ef39bafbae2b6e7f9b1be0ae9c73915b0c5c4
malvinh1 <malvin@pintu.co.id>
2023-11-05 15:40:40 +0700

    feat: init mongoose connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/paystonedb');
+
+const db = mongoose.connection;
+
+db.on('error', (error) => console.log(error));
+db.on('open', () => console.log('Connected to the Database'));
 
 app.listen(3000, () => console.log('Server connected'));
